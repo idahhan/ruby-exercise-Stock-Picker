eb69b784a49752f8c823d34daddaaee40dca27e3
idahhan <34948633+idahhan@users.noreply.github.com>
2022-04-26 11:55:35 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,26 +1,23 @@
-
 def stock_picker(priceList)
-  max=0
-  max_indexs=[]
+ max_profit=0
+  days=[]
   size=priceList.length+1
- results=Array.new(size,0) { Array.new(size,0)}
+ matrix=Array.new(size,0) { Array.new(size,0)}
   # profit matrix
   priceList.each_with_index do |price, day_index|
-   results.each_with_index do |profit, dayn_index|
+   matrix.each_with_index do |profit, dayn_index|
   # prevent buying on day n and selling on day -n
     next if dayn_index>day_index
     profit[day_index]= price - priceList[dayn_index]
-  # store max
-     if profit[day_index]>max
-       max=profit[day_index]
-       max_indexs[0]=dayn_index
-       max_indexs[1]=day_index
+  # store max_profit
+     if profit[day_index]>max_profit
+       max_profit=profit[day_index]
+       days[0]=dayn_index
+       days[1]=day_index
      end 
     end
   end
-  return max_indexs
+  return days
 end 
 
-
-stock_picker([1,100,20,40,60,150,7,8,1])
-
+p stock_picker([1,100,20,40,60,150,7,8,1])
